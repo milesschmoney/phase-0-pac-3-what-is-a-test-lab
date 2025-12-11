917bdfdb620c04d6f5672d3a5ae1864f1026a97f
Alex Ndaula <your_email@example.com>
2025-12-11 11:00:17 -0500

    Complete test lab

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const name = "Susan";
-const height = 39; // must be a NUMBER < 40
+const height = 39;
+
 const message = `${name} is ${height} inches tall`;
 
 module.exports = { name, height, message };
